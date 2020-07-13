478e0ad1a874c2c9efa03343918de9384432f85b
nakseongchoi <39376842+nakseongchoi@users.noreply.github.com>
2020-07-14 00:53:00 +0900

    2.18.Search Controller

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,7 +1,22 @@
-export const videoHome = (req, res) => res.render("videoHome");
-export const searchVideo = (req, res) => res.render("searchVideo");
-export const videos = (req, res) => res.render("videos");
-export const uploadVideo = (req, res) => res.render("uploadVideo");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const EditVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const videoHome = (req, res) => res.render("videoHome", {pageTitle : "Home", potato:1234 });
+
+export const searchVideo = (req, res) => {
+    // const {
+    //     query: { term : searchingBy }
+    // } = req;
+    // res.render("searchVideo", {pageTitle : "Search", searchingBy});
+        const searchingBy = req.query.term;    
+        // console.log(req.query);
+    res.render("searchVideo", {pageTitle : "Search", searchingBy : searchingBy});
+} ;
+
+
+export const videos = (req, res) => res.render("videos", {pageTitle : "Videos"});
+
+export const uploadVideo = (req, res) => res.render("uploadVideo", {pageTitle : "Upload"});
+
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle : "Video Detail"});
+
+export const EditVideo = (req, res) => res.render("editVideo", {pageTitle : "Edit Video"});
+
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle : "Delete Video"});
